21b443863ac06ab387a67efc7fa9923f82611d88
nikhilp132 <nikhilparadkar200@gmail.com>
2021-07-25 13:07:56 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 console.log('hello!');
 
-$(document).ready(function() {
-  // $('.mainContainer');
-});
+$(document).ready(function() {});
 
 function startGame() {
   $('.startButton').fadeOut();
-  $('.platform1').animate({ left: '10px' }, function() {
+  $('.platform1').animate({ left: '0px' }, function() {
     createBridge();
   });
 }
 
 function createBridge() {
-  var platformDist = Math.random();
-
-  console = $('.mainContainer').append(
-    '<div class="platforms"><div class="bridge bridge2"></div></div>'
+  var platformDist = Math.random() * 100;
+  var platformWidth = Math.random() * 100;
+  console.log(platformDist);
+  $('.mainContainer').append(
+    '<div class="platforms platform2"><div class="bridge bridge2"></div></div>'
   );
+
+  $('.platform2').css('margin-left', platformDist);
+  $('.platform2').css('width', platformWidth);
 }
