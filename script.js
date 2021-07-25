9a1832b8f99a9538ca803b7ef4a333e5ac012c6a
nikhilp132 <nikhilparadkar200@gmail.com>
2021-07-25 12:28:19 +0530

    3 files modified

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ $(document).ready(function() {
   // $('.mainContainer');
 });
 
+function startGame() {
+  $('.platform1').animate({ left: '10px' }, function() {
+    $('.startButton').css('display', 'none');
+    createBridge();
+  });
+}
+
 function createBridge() {
-  $('.mainContainer').append('<div class="bridge"></div>');
+  $('.mainContainer').append(
+    '<div class="platforms"><div class="bridge"></div></div>'
+  );
 }
